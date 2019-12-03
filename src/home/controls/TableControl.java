fc25f923e6d23916f06285d660dd4ee78f348ed7
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-04 03:31:28 +0900

    Modify TableControl

--- src/home/controls/TableControl.java
+++ src/home/controls/TableControl.java
@@ -1,103 +1,40 @@
 package home.controls;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.util.Vector;
 
+import home.daos.TableDao;
 import home.model.DirectoryModel;
 import home.model.LectureModel;
 import home.model.UserModel;
 
 public class TableControl{
 	
-    private Vector<DirectoryModel> campusModels;
-    private Vector<DirectoryModel> collegeModels;
-    private Vector<DirectoryModel> departmentModels;
-    private Vector<LectureModel> lectureModels;
+	TableDao tableDao = new TableDao();
 
     public UserModel checkCurrentUser() {
-    	Scanner scanner;
-		UserModel user = new UserModel();
-		try {
-			scanner = new Scanner(new File("data/user/CurrentUser"));
-	
-			while(scanner.hasNext()) {
-				user.setUserID(scanner.next());
-				user.setUserName(scanner.next());
-				user.setUserCollege(scanner.next());
-				user.setUserDepartment(scanner.next());
-				user.setUserNumber(scanner.next());		
-			}
-			scanner.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return user;
-
+		return tableDao.checkCurrentUser();
     }
     
     // Directory Methods
 
     // [Campus] File Read and Add Items
     public Vector<DirectoryModel> getCampusData(String fileName) throws FileNotFoundException {
-        campusModels = new Vector<DirectoryModel>();
-
-        Scanner scanner = new Scanner(new File(fileName));
-
-        while(scanner.hasNext()) {
-            DirectoryModel campusModel = new DirectoryModel();
-            campusModel.read(scanner);
-            campusModels.add(campusModel);
-        }
-        scanner.close();
-        return campusModels;
+        return tableDao.getCampusData(fileName);
     }
 
-    
     // [College] File Read and Add Items
     public Vector<DirectoryModel> getCollegeData(String fileName) throws FileNotFoundException {
-        collegeModels = new Vector<DirectoryModel>();
-
-        Scanner scanner = new Scanner(new File(fileName));
-
-        while(scanner.hasNext()) {
-            DirectoryModel collegeModel = new DirectoryModel();
-            collegeModel.read(scanner);
-            collegeModels.add(collegeModel);
-        }
-        scanner.close();
-        return collegeModels;
+        return tableDao.getCollegeData(fileName);
     }
 
     // [Department] File Read and Add Items
     public Vector<DirectoryModel> getDepartmentData(String fileName) throws FileNotFoundException {
-        departmentModels = new Vector<DirectoryModel>();
-
-        Scanner scanner = new Scanner(new File(fileName));
-
-        while(scanner.hasNext()) {
-            DirectoryModel departmentModel = new DirectoryModel();
-            departmentModel.read(scanner);
-            departmentModels.add(departmentModel);
-        }
-        scanner.close();
-        return departmentModels;
+        return tableDao.getDepartmentData(fileName);
     }
 
     // Lecture Methods
-
     public Vector<LectureModel> getLectureData(String fileName) throws FileNotFoundException{
-        lectureModels = new Vector<LectureModel>();
-
-        Scanner scanner = new Scanner(new File(fileName));
-
-        while(scanner.hasNext()) {
-            LectureModel lectureModel = new LectureModel();
-            lectureModel.read(scanner);
-            lectureModels.add(lectureModel);
-        }
-        scanner.close();
-        return lectureModels;
+        return tableDao.getLectureData(fileName);
     }
 }
