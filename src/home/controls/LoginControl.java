2eaaeb571d6f6e335ba7e64ee912ab7dc3c10b91
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-04 03:05:50 +0900

    Modify Login Control and Dao

--- src/home/controls/LoginControl.java
+++ src/home/controls/LoginControl.java
@@ -1,44 +1,16 @@
 package home.controls;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-import home.model.UserModel;
+import home.daos.LoginDao;
 
 public class LoginControl {
-
-	UserModel user = new UserModel();
 	
-	public UserModel getUser() {
-		return user;
-	}
-	
-	// Login DB 읽기 전용 메소드
-	public void read(Scanner scanner) {
-		user.setUserID(scanner.next());
-		user.setUserPassword(scanner.next());
-		user.setUserName(scanner.next());
-		user.setUserCollege(scanner.next());
-		user.setUserDepartment(scanner.next());
-		user.setUserNumber(scanner.next());
-	}
+	LoginDao loginDao = new LoginDao();
 	
 	// Login DB와 사용자 입력값 비교 메소드
-	public boolean authenticate(String inputID, String inputPassword) throws FileNotFoundException{
-		Scanner scanner;
-		scanner = new Scanner(new File("data/user/Login"));
-			
-		while(scanner.hasNext()) {
-			this.read(scanner);
-
-			if(user.getUserID().equals(inputID)&&user.getUserPassword().equals(inputPassword)) {
-				scanner.close();
-				return true;
-			}
+		public boolean authenticate(String inputID, String inputPassword) throws FileNotFoundException{
+			return loginDao.authenticate(inputID, inputPassword);
 		}
-		scanner.close();
-		return false;
-	}
 	
 }
